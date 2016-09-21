27eb0a48c0fb4222203257c54aa25f4e029af98c
Will Palmer <will@do-it-well.co.uk>
2016-09-21 02:56:03 +0100

    add configuration dsl gem

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'cloudformation-ruby-dsl'
+gem 'diw-config', :git => 'https://github.com/wpalmer/config-ruby-dsl.git'
--- sample.rb
+++ sample.rb
@@ -1,9 +1,15 @@
 #!/usr/bin/env ruby
 require 'bundler/setup'
 require 'cloudformation-ruby-dsl/cfntemplate'
+require 'diw/config'
+STDERR.puts ARGV.inspect
+
+cfg = DIW::Config::Config.new do
+  var "testo", "boop"
+end
 
 template = template do
-  resource "HelloBucket", :Type => "AWS::S3::Bucket"
+  resource "HelloBucket", :Type => "AWS::S3::Bucket", :Properties => cfg.testo
 end
 
 template.exec!
